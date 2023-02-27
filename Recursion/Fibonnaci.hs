3a21b51f27a1103c40109e1b133c086217537367
john <john@penguin>
2023-02-26 20:53:30 -0500

    use where for readability

--- Recursion/Fibonnaci.hs
+++ Recursion/Fibonnaci.hs
@@ -1,8 +1,11 @@
-fibonnaciA :: [Int] -> Int -> (Int, Int) -> [Int]
-fibonnaciA xs 0 _ = xs
-fibonnaciA xs count (n1, n2) = fibonnaciA (xs ++ [n1 + n2]) (count - 1) ((n1 + n2), n1)
+fibonnaci' :: [Int] -> Int -> (Int, Int) -> [Int]
+fibonnaci' xs 0 _ = xs
+fibonnaci' seed count memo = fibonnaci' (seed ++ [next]) (count - 1) (next, last)
+  where
+    (last, secondLast) = memo
+    next = last + secondLast
 
 fibonnaci :: Int -> [Int]
 fibonnaci 0 = [0]
 fibonnaci 1 = [0, 1]
-fibonnaci x = fibonnaciA (fibonnaci 1) (x - 1) (1, 0)
+fibonnaci x = fibonnaci' (fibonnaci 1) (x - 1) (1, 0)
