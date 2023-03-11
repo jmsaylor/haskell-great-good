7dcba40e264cb5e4b12705d6210c9b1767e41073
john <john@penguin>
2023-03-11 10:38:30 -0500

    Caesar's Ciper

---
+++ Modules/CaesarCipher.hs
@@ -0,0 +1,7 @@
+import Data.Char
+
+encode :: Int -> String -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
