446f4c5c1aec83f9994bb5d1e2ca748b8b47af65
john <john@penguin>
2023-03-12 17:21:02 -0400

    Map.map stringToInt

--- Modules/Map.hs
+++ Modules/Map.hs
@@ -1,4 +1,11 @@
 import qualified Data.Map as Map
+import Data.Char
 
-phonebook :: Map.Map String String
-phonebook = Map.fromList [("alice", "555-1234"), ("bob", "555-5678"), ("charlie", "555-9012")]
+stringToDigits :: String -> [Int]
+stringToDigits = map digitToInt . filter isDigit
+
+phonebook' :: Map.Map String String
+phonebook' = Map.fromList [("alice", "555-1234"), ("bob", "555-5678"), ("charlie", "555-9012")]
+
+phonebook :: Map.Map String [Int]
+phonebook = Map.map stringToDigits phonebook'
